edc364107393af5e9615b699cf195d89f2142b51
Jealyang <jaelyangchoi@gmail.com>
2021-01-16 23:48:45 +0900

    router 도입

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ const app = express(); //함수. application 객체 반환
 const port = 3000;
 const fs = require("fs");
 const template = require("./lib/template.js");
-const path = require("path");
-const sanitizeHtml = require("sanitize-html");
-const qs = require("querystring");
+const topicRouter = require("./routes/topic"); //라우터 모듈(미들웨어)
 
 //정적 파일
 app.use(express.static("public")); //public 디렉토리에서 static 파일 찾겠다
@@ -24,6 +22,7 @@ let myLogger = function (req, res, next) {
 app.use(myLogger); //요청을 받을 때마다 라우팅 전에 콘솔에 "LOGGED" 출력
 app.use(bodyParser.urlencoded({ extended: false })); //body-parser가 실행되어 미들웨어 장착
 app.use(compression());
+
 //get 요청에 대해서만 적용되는 미들웨어!
 app.get("*", (req, res, next) => {
   fs.readdir("./data", function (error, filelist) {
@@ -31,20 +30,8 @@ app.get("*", (req, res, next) => {
     next();
   });
 });
-/*
-//route, routing : path마다 적당한 응답. (기존 코드는 if문으로 구현)
-//라우팅: 애플리케이션 엔드 포인트(URI)의 정의, 그리고 URI가 클라이언트 요청에 응답하는 방식
-//기본 형식
-app.get("/routing", (req, res) => {
-  res.send("routing!!");
-});
 
-//clean url : clean URL은 질의어 없이, 경로만 가진 간단한 구조의 URL. 검색 엔진 친화적.
-//기본 형식
-app.get("/users/:userId/books/:bookId", (req, res) => {
-  //http://localhost:3000/users/cjl0701/books/1
-  res.send(req.params); //{"userId":"cjl0701","bookId":"1"}
-});*/
+app.use("/topic", topicRouter); //이 경로에 topicRouter를 미들웨어로서 할당
 
 app.get("/", (req, res) => {
   //사실은 이 콜백 함수도 미들웨어였다!
@@ -61,124 +48,6 @@ app.get("/", (req, res) => {
   // response.writeHead(200);
   // response.end(html);
   res.send(html);
-});
-
-app.get("/topic/create", (req, res) => {
-  let title = "WEB - create";
-  let list = template.list(req.list);
-  let html = template.HTML(
-    title,
-    list,
-    `
-    <form action="/topic/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-    `,
-    ""
-  );
-  res.send(html);
-});
-
-app.post("/topic/create_process", (req, res) => {
-  /*
-  let body = "";
-  req.on("data", function (data) { //data 이벤트
-    body = body + data; // 패킷
-  });
-  req.on("end", function () { //end 이벤트
-    let post = qs.parse(body);
-    let title = post.title;
-    let description = post.description;
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      res.redirect(`/page/${title}`);
-    });
-  });*/
-  let post = req.body;
-  let title = post.title;
-  let description = post.description;
-  fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-    // res.writeHead(302, { Location: `/page/${title}` });
-    // res.end();
-    res.redirect(`/topic/page/${title}`);
-  });
-});
-
-app.get("/topic/update/:pageId", (req, res) => {
-  let title = req.params.pageId;
-  fs.readFile(`data/${title}`, "utf8", function (err, description) {
-    let list = template.list(req.list);
-    let html = template.HTML(
-      title,
-      list,
-      `
-      <form action="/topic/update_process" method="post">
-        <input type="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p>
-          <textarea name="description" placeholder="description">${description}</textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-    );
-    res.send(html);
-  });
-});
-
-app.post("/topic/update_process", (req, res) => {
-  let post = req.body;
-  let id = post.id;
-  let title = post.title;
-  let description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      res.redirect(`/topic/page/${title}`);
-    });
-  });
-});
-
-app.post("/topic/delete_process", (req, res) => {
-  let post = req.body;
-  let id = post.id;
-  let filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    res.redirect("/");
-  });
-});
-
-app.get("/topic/page/:pageId", (req, res, next) => {
-  let filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    if (err) next(err);
-    else {
-      let sanitizedTitle = sanitizeHtml(filteredId);
-      let sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"],
-      });
-      let list = template.list(req.list);
-      let html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-        <a href="/topic/update/${sanitizedTitle}">update</a>
-        <form action="/topic/delete_process" method="post">
-          <input type="hidden" name="id" value="${sanitizedTitle}">
-          <input type="submit" value="delete">
-        </form>`
-      );
-      res.send(html);
-    }
-  });
 });
 
 //미들웨어는 순차적으로 처리되기 때문에 맨 밑에!
