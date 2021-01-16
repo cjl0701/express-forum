4db5a2fb2a78563c6ceddd0414c0e3d52b43e644
Jealyang <jaelyangchoi@gmail.com>
2021-01-16 12:42:21 +0900

    middle ware 작성 및 사용

--- main.js
+++ main.js
@@ -6,14 +6,28 @@ const template = require("./lib/template.js");
 const path = require("path");
 const sanitizeHtml = require("sanitize-html");
 const qs = require("querystring");
-const bodyParser = require("body-parser"); //미들웨어
+
+//third-party 미들웨어 import
+const bodyParser = require("body-parser");
 const compression = require("compression");
 
-//요청이 들어올 때마다 실행됨
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false })); //body-parser가 실행되어 미들웨어 장착
+//미들웨어 작성
+let myLogger = function (req, res, next) {
+  console.log("LOGGED");
+  next(); //앱 내의 다음 미들웨어 함수 호출.
+};
 
-app.use(compression()); //compressiong()함수 실행 미들웨어 리턴 -> app은 미들웨어 장착
+//미들웨어 함수 로드. 요청이 들어올 때마다 실행됨
+app.use(myLogger); //요청을 받을 때마다 라우팅 전에 콘솔에 "LOGGED" 출력
+app.use(bodyParser.urlencoded({ extended: false })); //body-parser가 실행되어 미들웨어 장착
+app.use(compression());
+//get 요청에 대해서만 적용되는 미들웨어!
+app.get("*", (req, res, next) => {
+  fs.readdir("./data", function (error, filelist) {
+    req.list = filelist;
+    next();
+  });
+});
 
 //route, routing : path마다 적당한 응답. (기존 코드는 if문으로 구현)
 //라우팅: 애플리케이션 엔드 포인트(URI)의 정의, 그리고 URI가 클라이언트 요청에 응답하는 방식
@@ -30,69 +44,64 @@ app.get("/users/:userId/books/:bookId", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  fs.readdir("./data", function (error, filelist) {
-    let title = "Welcome";
-    let description = "Hello, Node.js";
-    let list = template.list(filelist);
-    let html = template.HTML(
-      title,
-      list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    );
-    // response.writeHead(200);
-    // response.end(html);
-    res.send(html);
-  });
+  //사실은 이 콜백 함수도 미들웨어였다!
+  let title = "Welcome";
+  let description = "Hello, Node.js";
+  let list = template.list(req.list);
+  let html = template.HTML(
+    title,
+    list,
+    `<h2>${title}</h2>${description}`,
+    `<a href="/create">create</a>`
+  );
+  // response.writeHead(200);
+  // response.end(html);
+  res.send(html);
 });
 
 app.get("/page/:pageId", (req, res) => {
-  fs.readdir("./data", function (error, filelist) {
-    let filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-      let sanitizedTitle = sanitizeHtml(filteredId);
-      let sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"],
-      });
-      let list = template.list(filelist);
-      let html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      res.send(html);
+  let filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+    let sanitizedTitle = sanitizeHtml(filteredId);
+    let sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: ["h1"],
     });
+    let list = template.list(req.list);
+    let html = template.HTML(
+      sanitizedTitle,
+      list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      ` <a href="/create">create</a>
+        <a href="/update/${sanitizedTitle}">update</a>
+        <form action="/delete_process" method="post">
+          <input type="hidden" name="id" value="${sanitizedTitle}">
+          <input type="submit" value="delete">
+        </form>`
+    );
+    res.send(html);
   });
 });
 
 app.get("/create", (req, res) => {
-  fs.readdir("./data", function (error, filelist) {
-    let title = "WEB - create";
-    let list = template.list(filelist);
-    let html = template.HTML(
-      title,
-      list,
-      `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
+  let title = "WEB - create";
+  let list = template.list(req.list);
+  let html = template.HTML(
+    title,
+    list,
+    `
+    <form action="/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
     `,
-      ""
-    );
-    res.send(html);
-  });
+    ""
+  );
+  res.send(html);
 });
 
 app.post("/create_process", (req, res) => {
@@ -120,29 +129,27 @@ app.post("/create_process", (req, res) => {
 });
 
 app.get("/update/:pageId", (req, res) => {
-  fs.readdir("./data", function (error, filelist) {
-    let title = req.params.pageId;
-    fs.readFile(`data/${title}`, "utf8", function (err, description) {
-      let list = template.list(filelist);
-      let html = template.HTML(
-        title,
-        list,
-        `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-      );
-      res.send(html);
-    });
+  let title = req.params.pageId;
+  fs.readFile(`data/${title}`, "utf8", function (err, description) {
+    let list = template.list(req.list);
+    let html = template.HTML(
+      title,
+      list,
+      `
+      <form action="/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+          <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `,
+      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+    );
+    res.send(html);
   });
 });
 
