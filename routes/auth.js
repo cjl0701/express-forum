44b7b5fd4f7e9eee35703497a1d7ecba61bf561f
Jealyang <jaelyangchoi@gmail.com>
2021-01-18 22:32:35 +0900

    session store에 바로 반영하는 코드 추가

--- routes/auth.js
+++ routes/auth.js
@@ -31,8 +31,12 @@ router.post("/login_process", (req, res) => {
   let post = req.body; //by body-parser
   if (post.email === authData.email && post.password === authData.password) {
     req.session.is_logined = true;
-    req.session.nickname = authData.nickname;
-    res.redirect("/");
+    req.session.nickname = authData.nickname; //이는 나중에 세션 스토어에 반영된다.
+    //세션 스토어에 저장하는게 리다이렉션보다 느리면 로그인이 안된다.
+    //세션 스토어에 바로 저장하게 하는 메소드
+    req.session.save(function () {
+      res.redirect("/");
+    });
   } else res.send("who?");
 });
 
