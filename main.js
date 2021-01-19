9b6ce4b36acb3788932395f9477cb8e2df6fd2ac
Jealyang <jaelyangchoi@gmail.com>
2021-01-19 19:41:45 +0900

    인증 미들웨어 passport를 이용한 로그인 구현

--- main.js
+++ main.js
@@ -5,20 +5,15 @@ const fs = require("fs");
 const helmet = require("helmet");
 const session = require("express-session"); //세션 미들웨어는 request 객체의 프로퍼티로 session 객체 추가
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 //third-party 미들웨어 import
 const bodyParser = require("body-parser");
 const compression = require("compression");
-//미들웨어 작성
-let myLogger = function (req, res, next) {
-  console.log("LOGGED");
-  next(); //앱 내의 다음 미들웨어 함수 호출.
-};
 
 //미들웨어 함수 로드. 요청이 들어올 때마다 실행됨
 app.use(helmet()); //보안
-//정적 파일
 app.use(express.static("public")); //public 디렉토리에서 static 파일 찾겠다
-app.use(myLogger); //요청을 받을 때마다 라우팅 전에 콘솔에 "LOGGED" 출력
 app.use(bodyParser.urlencoded({ extended: false })); //body-parser가 실행되어 미들웨어 장착
 app.use(compression());
 app.use(
@@ -28,6 +23,54 @@ app.use(
     saveUninitialized: true, //세션이 필요하기 전까진 구동x
     store: new FileStore(),
   })
+);
+//학습용. 실서버가 이렇게 하면 안된다.
+const authData = {
+  email: "cjl2076@naver.com",
+  pwd: "0000",
+  nickname: "Owner",
+};
+
+//passort는 session을 필요로 하기 때문에 session 아래에!
+app.use(passport.initialize());
+app.use(passport.session()); //내부적으로 세션을 사용하겠다.
+
+//세션을 처리하는 방법
+//user의 식별자를 넘겨서 세션에 저장
+passport.serializeUser(function (user, done) {
+  console.log("serializeUser", user);
+  done(null, user.email);
+});
+//user의 식별자를 id로 받아서 DB에서 user의 정보 조회. 매 페이지에 접속할 때마다 작동
+passport.deserializeUser(function (id, done) {
+  console.log("deserializeUser", id);
+  done(null, authData);
+});
+
+passport.use(
+  //local: id & pwd를 사용하는 strategy(인증 수단)
+  new LocalStrategy(
+    //기본적으로 LocalStrategy는 username및 라는 매개 변수에서 자격 증명을 찾음. 바꾸려면.
+    {
+      usernameField: "email",
+      passwordField: "pwd",
+    },
+    function (username, password, done) {
+      if (username === authData.email)
+        if (password === authData.pwd) return done(null, authData);
+        else return done(null, false, { message: "Incorrect password." });
+      else return done(null, false, { message: "Incorrect username." });
+    }
+  )
+);
+
+app.post(
+  "/auth/login_process",
+  passport.authenticate("local", {
+    //successRedirect: "/",
+    failureRedirect: "/auth/login",
+  }),
+  (req, res) => req.session.save(() => res.redirect("/"))
 );
 
 //get 요청에 대해서만 적용되는 미들웨어!
--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,6 @@ const express = require("express");
 const router = express.Router();
 const template = require("../lib/template.js");
 
-//학습용. 실서버가 이렇게 하면 안된다.
-const authData = {
-  email: "cjl2076@naver.com",
-  password: "0000",
-  nickname: "Owner",
-};
-
 router.get("/login", (req, res) => {
   let title = "WEB - login";
   let list = template.list(req.list);
@@ -18,7 +11,7 @@ router.get("/login", (req, res) => {
     `
     <form action="/auth/login_process" method="post">
       <p><input type="text" name="email" placeholder="email"></p>
-      <p><input type="password" name="password" placeholder="password"></p>
+      <p><input type="password" name="pwd" placeholder="password"></p>
       <p><input type="submit" value="login"></p>
     </form>
     `,
@@ -27,6 +20,7 @@ router.get("/login", (req, res) => {
   res.send(html);
 });
 
+/*
 router.post("/login_process", (req, res) => {
   let post = req.body; //by body-parser
   if (post.email === authData.email && post.password === authData.password) {
@@ -39,6 +33,7 @@ router.post("/login_process", (req, res) => {
     });
   } else res.send("who?");
 });
+*/
 
 router.get("/logout", (req, res) => {
   req.session.destroy(function (err) {
@@ -47,118 +42,3 @@ router.get("/logout", (req, res) => {
 });
 
 module.exports = router;
-/*
-//router 객체에 미들웨어 설치
-router.get("/create", (req, res) => {
-  let title = "WEB - create";
-  let list = template.list(req.list);
-  let html = template.HTML(
-    title,
-    list,
-    `
-     <form action="/topic/create_process" method="post">
-       <p><input type="text" name="title" placeholder="title"></p>
-       <p><textarea name="description" placeholder="description"></textarea></p>
-       <p><input type="submit"></p>
-     </form>
-    `,
-    ""
-  );
-  res.send(html);
-});
-
-router.post("/create_process", (req, res) => {
-    // let body = "";
-    // req.on("data", function (data) { //data 이벤트
-    //   body = body + data; // 패킷
-    // });
-    // req.on("end", function () { //end 이벤트
-    //   let post = qs.parse(body);
-    //   let title = post.title;
-    //   let description = post.description;
-    //   fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-    //     res.redirect(`/page/${title}`);
-    //   });
-    // });
-  let post = req.body;
-  let title = post.title;
-  let description = post.description;
-  fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-    // res.writeHead(302, { Location: `/page/${title}` });
-    // res.end();
-    res.redirect(`/topic/${title}`);
-  });
-});
-
-router.get("/update/:pageId", (req, res) => {
-  let title = req.params.pageId;
-  fs.readFile(`data/${title}`, "utf8", function (err, description) {
-    let list = template.list(req.list);
-    let html = template.HTML(
-      title,
-      list,
-      `
-        <form action="/topic/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-    );
-    res.send(html);
-  });
-});
-
-router.post("/update_process", (req, res) => {
-  let post = req.body;
-  let id = post.id;
-  let title = post.title;
-  let description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      res.redirect(`/topic/${title}`);
-    });
-  });
-});
-
-router.post("/delete_process", (req, res) => {
-  let post = req.body;
-  let id = post.id;
-  let filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    res.redirect("/");
-  });
-});
-
-router.get("/:pageId", (req, res, next) => {
-  let filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    if (err) next(err);
-    else {
-      let sanitizedTitle = sanitizeHtml(filteredId);
-      let sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"],
-      });
-      let list = template.list(req.list);
-      let html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-          <a href="/topic/update/${sanitizedTitle}">update</a>
-          <form action="/topic/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      res.send(html);
-    }
-  });
-});
-*/
