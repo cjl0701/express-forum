d5063f27b9353bacfe6505e56d49cbae9c93d940
Jealyang <jaelyangchoi@gmail.com>
2021-01-20 14:26:28 +0900

    회원가입 구현

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const template = require("../lib/template.js");
+const shortid = require("shortid");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+db.defaults({ users: [] }).write();
 
 module.exports = function (passport) {
   router.get("/login", (req, res) => {
@@ -69,6 +75,48 @@ router.post("/login_process", (req, res) => {
     // req.session.destroy(function (err) {
     //   res.redirect("/");
     // });
+  });
+
+  router.get("/register", (req, res) => {
+    let title = "WEB - register";
+    let list = template.list(req.list);
+    let fmsg = req.flash();
+    let feedback = "";
+    if (fmsg.message) feedback = fmsg.message;
+    let html = template.HTML(
+      title,
+      list,
+      `
+      <div style="color:red;">${feedback}</div>
+      <form action="/auth/register_process" method="post">
+        <p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="password" name="pwd" placeholder="password"></p>
+        <p><input type="password" name="pwd2" placeholder="password"></p>
+        <p><input type="text" name="displayName" placeholder="display name"></p>
+        <p><input type="submit" value="register"></p>
+      </form>
+      `,
+      ""
+    );
+    res.send(html);
+  });
+
+  router.post("/register_process", (req, res) => {
+    let post = req.body;
+    if (post.pwd !== post.pwd2) {
+      req.flash("message", "password must be same!");
+      req.session.save(() => res.redirect("/auth/register"));
+    } else {
+      db.get("users")
+        .push({
+          id: shortid.generate(),
+          email: post.email,
+          pwd: post.pwd,
+          nickname: post.displayName,
+        })
+        .write();
+      res.redirect("/");
+    }
   });
 
   return router;
