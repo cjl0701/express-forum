198e3871d63f967ba4b23f75d364ee176f8d1a85
Jealyang <jaelyangchoi@gmail.com>
2021-01-25 17:41:25 +0900

    google oauth 추가

--- main.js
+++ main.js
@@ -64,29 +64,28 @@ app.listen(port, () => {
 });
 
 /* 이 node.js 기반 코드를 express로 재구성할 것
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var qs = require("querystring");
+var template = require("./lib/template.js");
+var path = require("path");
+var sanitizeHtml = require("sanitize-html");
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      } else {
-    } else if(pathname === '/create'){
-    } else if(pathname === '/create_process'){
-    } else if(pathname === '/update'){
-    } else if(pathname === '/update_process'){
-    } else if(pathname === '/delete_process'){
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname === "/") {
+  } else if (pathname === "/create") {
+  } else if (pathname === "/create_process") {
+  } else if (pathname === "/update") {
+  } else if (pathname === "/update_process") {
+  } else if (pathname === "/delete_process") {
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
 });
 app.listen(3000);
 */
